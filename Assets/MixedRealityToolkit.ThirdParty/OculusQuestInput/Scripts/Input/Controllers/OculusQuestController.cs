7e074c32432d13dac194316270085ce4d9d40a72
Eric <eprovencher92@gmail.com>
2019-12-31 15:28:16 -0500

    First pass implementing controller / hand hotswap

---
+++ Assets/MixedRealityToolkit.ThirdParty/OculusQuestInput/Scripts/Input/Controllers/OculusQuestController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace prvncher.MixedReality.Toolkit.OculusQuestInput
+{
+    [MixedRealityController(SupportedControllerType.OculusTouch, new[] { Handedness.Left, Handedness.Right })]
+    public class OculusQuestController : BaseController
+    {
+        private MixedRealityPose currentPointerPose = MixedRealityPose.ZeroIdentity;
+
+
+        private MixedRealityPose currentIndexPose = MixedRealityPose.ZeroIdentity;
+        private MixedRealityPose currentGripPose = MixedRealityPose.ZeroIdentity;
+
+        private const float cTriggerDeadZone = 0.1f;
+
+        // TODO: Hand mesh
+        // private int[] handMeshTriangleIndices = null;
+        // private Vector2[] handMeshUVs;
+
+        public OculusQuestController(TrackingState trackingState, Handedness controllerHandedness, IMixedRealityInputSource inputSource = null, MixedRealityInteractionMapping[] interactions = null)
+            : base(trackingState, controllerHandedness, inputSource, interactions)
+        {
+        }
+
+        /// <summary>
+        /// The Windows Mixed Reality Controller default interactions.
+        /// </summary>
+        /// <remarks>A single interaction mapping works for both left and right controllers.</remarks>
+        public override MixedRealityInteractionMapping[] DefaultInteractions => new[]
+        {
+            new MixedRealityInteractionMapping(0, "Spatial Pointer", AxisType.SixDof, DeviceInputType.SpatialPointer),
+            new MixedRealityInteractionMapping(1, "Spatial Grip", AxisType.SixDof, DeviceInputType.SpatialGrip),
+            new MixedRealityInteractionMapping(2, "Grip Press", AxisType.SingleAxis, DeviceInputType.TriggerPress),
+            new MixedRealityInteractionMapping(3, "Trigger Position", AxisType.SingleAxis, DeviceInputType.Trigger),
+            new MixedRealityInteractionMapping(4, "Trigger Touch", AxisType.Digital, DeviceInputType.TriggerTouch),
+            new MixedRealityInteractionMapping(5, "Trigger Press (Select)", AxisType.Digital, DeviceInputType.Select),
+            new MixedRealityInteractionMapping(6, "Touchpad Position", AxisType.DualAxis, DeviceInputType.Touchpad),
+            new MixedRealityInteractionMapping(7, "Touchpad Touch", AxisType.Digital, DeviceInputType.TouchpadTouch),
+            new MixedRealityInteractionMapping(8, "Touchpad Press", AxisType.Digital, DeviceInputType.TouchpadPress),
+            new MixedRealityInteractionMapping(9, "Menu Press", AxisType.Digital, DeviceInputType.Menu),
+            new MixedRealityInteractionMapping(10, "Thumbstick Position", AxisType.DualAxis, DeviceInputType.ThumbStick),
+            new MixedRealityInteractionMapping(11, "Thumbstick Press", AxisType.Digital, DeviceInputType.ThumbStickPress),
+        };
+
+        public override void SetupDefaultInteractions(Handedness controllerHandedness)
+        {
+            AssignControllerMappings(DefaultInteractions);
+        }
+
+        /// <summary>
+        /// Update the controller data from the provided platform state
+        /// </summary>
+        /// <param name="interactionSourceState">The InteractionSourceState retrieved from the platform</param>
+        public void UpdateController(OVRCameraRig ovrRigRoot, OVRInput.Controller ovrController)
+        {
+            if (!Enabled || ovrRigRoot == null)
+            {
+                return;
+            }
+
+            IsPositionAvailable = OVRInput.GetControllerPositionTracked(ovrController);
+            IsRotationAvailable = OVRInput.GetControllerPositionTracked(ovrController);
+
+            Transform playSpaceTransform = ovrRigRoot.transform;
+
+            // Update transform
+            Vector3 localPosition = OVRInput.GetLocalControllerPosition(ovrController);
+            Vector3 worldPosition = playSpaceTransform.TransformPoint(localPosition);
+            // Debug.Log("Controller " + Handedness + " - local: " + localPosition + " - world: " + worldPosition);
+
+            Quaternion localRotation = OVRInput.GetLocalControllerRotation(ovrController);
+            Quaternion worldRotation = playSpaceTransform.rotation * localRotation;
+
+            // Update velocity
+            Vector3 localVelocity = OVRInput.GetLocalControllerVelocity(ovrController);
+            Velocity = playSpaceTransform.TransformDirection(localVelocity);
+
+            Vector3 localAngularVelocity = OVRInput.GetLocalControllerAngularVelocity(ovrController);
+            AngularVelocity = playSpaceTransform.TransformDirection(localAngularVelocity);
+
+            if (IsPositionAvailable)
+            {
+                currentPointerPose.Position = currentGripPose.Position = worldPosition;
+            }
+
+            if (IsRotationAvailable)
+            {
+                currentPointerPose.Rotation = currentGripPose.Rotation = worldRotation;
+            }
+
+            // Todo: Complete touch controller mapping
+
+            bool isTriggerPressed = false;
+            if (ControllerHandedness == Handedness.Left)
+            {
+                isTriggerPressed = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > cTriggerDeadZone;
+            }
+            else
+            {
+                isTriggerPressed = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > cTriggerDeadZone;
+            }
+
+            for (int i = 0; i < Interactions?.Length; i++)
+            {
+                switch (Interactions[i].InputType)
+                {
+                    case DeviceInputType.SpatialPointer:
+                        Interactions[i].PoseData = currentPointerPose;
+                        if (Interactions[i].Changed)
+                        {
+                            CoreServices.InputSystem?.RaisePoseInputChanged(InputSource, ControllerHandedness, Interactions[i].MixedRealityInputAction, currentPointerPose);
+                        }
+                        break;
+                    case DeviceInputType.SpatialGrip:
+                        Interactions[i].PoseData = currentGripPose;
+                        if (Interactions[i].Changed)
+                        {
+                            CoreServices.InputSystem?.RaisePoseInputChanged(InputSource, ControllerHandedness, Interactions[i].MixedRealityInputAction, currentGripPose);
+                        }
+                        break;
+                    case DeviceInputType.Select:
+                        Interactions[i].BoolData = isTriggerPressed;
+
+                        if (Interactions[i].Changed)
+                        {
+                            if (Interactions[i].BoolData)
+                            {
+                                CoreServices.InputSystem?.RaiseOnInputDown(InputSource, ControllerHandedness, Interactions[i].MixedRealityInputAction);
+                            }
+                            else
+                            {
+                                CoreServices.InputSystem?.RaiseOnInputUp(InputSource, ControllerHandedness, Interactions[i].MixedRealityInputAction);
+                            }
+                        }
+                        break;
+                    case DeviceInputType.TriggerPress:
+                        Interactions[i].BoolData = isTriggerPressed;
+
+                        if (Interactions[i].Changed)
+                        {
+                            if (Interactions[i].BoolData)
+                            {
+                                CoreServices.InputSystem?.RaiseOnInputDown(InputSource, ControllerHandedness, Interactions[i].MixedRealityInputAction);
+                            }
+                            else
+                            {
+                                CoreServices.InputSystem?.RaiseOnInputUp(InputSource, ControllerHandedness, Interactions[i].MixedRealityInputAction);
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit.ThirdParty/OculusQuestInput/Scripts/Input/OculusQuestInputManager.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace prvncher.MixedReality.Toolkit.OculusQuestInput
+{
+    /// <summary>
+    /// Manages Oculus Quest Hand Inputs
+    /// </summary>
+    [MixedRealityDataProvider(typeof(IMixedRealityInputSystem), SupportedPlatforms.Android | SupportedPlatforms.WindowsEditor | SupportedPlatforms.MacEditor | SupportedPlatforms.LinuxEditor, "Oculus Quest Hand Input Manager")]
+    public class OculusQuestInputManager : BaseInputDeviceManager, IMixedRealityCapabilityCheck
+    {
+        private Dictionary<Handedness, OculusQuestHand> trackedHands = new Dictionary<Handedness, OculusQuestHand>();
+        private Dictionary<Handedness, OculusQuestController> trackedControllers = new Dictionary<Handedness, OculusQuestController>();
+
+        private OVRCameraRig cameraRig;
+
+        private OVRHand rightHand;
+        private OVRSkeleton rightSkeleton;
+
+        private OVRHand leftHand;
+        private OVRSkeleton leftSkeleton;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="registrar">The <see cref="IMixedRealityServiceRegistrar"/> instance that loaded the data provider.</param>
+        /// <param name="inputSystem">The <see cref="Microsoft.MixedReality.Toolkit.Input.IMixedRealityInputSystem"/> instance that receives data from this provider.</param>
+        /// <param name="name">Friendly name of the service.</param>
+        /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The service's configuration profile.</param>
+        public OculusQuestInputManager(
+            IMixedRealityInputSystem inputSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : base(inputSystem, name, priority, profile)
+        {
+        }
+
+        public override void Enable()
+        {
+            base.Enable();
+
+            cameraRig = GameObject.FindObjectOfType<OVRCameraRig>();
+            var ovrHands = cameraRig.GetComponentsInChildren<OVRHand>();
+
+            foreach (var ovrHand in ovrHands)
+            {
+                var skeltonDataProvider = ovrHand as OVRSkeleton.IOVRSkeletonDataProvider;
+                var skeltonType = skeltonDataProvider.GetSkeletonType();
+
+                var ovrSkelton = ovrHand.GetComponent<OVRSkeleton>();
+                if (ovrSkelton == null)
+                {
+                    continue;
+                }
+
+                switch (skeltonType)
+                {
+                    case OVRSkeleton.SkeletonType.HandLeft:
+                        leftHand = ovrHand;
+                        leftSkeleton = ovrSkelton;
+                        break;
+                    case OVRSkeleton.SkeletonType.HandRight:
+                        rightHand = ovrHand;
+                        rightSkeleton = ovrSkelton;
+                        break;
+                }
+            }
+        }
+
+        public override void Disable()
+        {
+            base.Disable();
+
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+
+            foreach (var hand in trackedHands)
+            {
+                if (hand.Value != null)
+                {
+                    inputSystem?.RaiseSourceLost(hand.Value.InputSource, hand.Value);
+                }
+            }
+
+            trackedHands.Clear();
+        }
+
+        public override IMixedRealityController[] GetActiveControllers()
+        {
+            return trackedHands.Values.ToArray<IMixedRealityController>();
+        }
+
+        /// <inheritdoc />
+        public bool CheckCapability(MixedRealityCapability capability)
+        {
+            return (capability == MixedRealityCapability.ArticulatedHand);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (OVRPlugin.GetHandTrackingEnabled())
+            {
+                RemoveAllControllerDevices();
+                UpdateHands();
+            }
+            else
+            {
+                RemoveAllHandDevices();
+                UpdateControllers();
+            }
+        }
+
+        #region Controller Management
+        protected void UpdateControllers()
+        {
+            UpdateController(OVRInput.Controller.LTouch, Handedness.Left);
+            UpdateController(OVRInput.Controller.RTouch, Handedness.Left);
+        }
+
+        protected void UpdateController(OVRInput.Controller controller, Handedness handedness)
+        {
+            if (OVRInput.IsControllerConnected(controller))
+            {
+                var touchController = GetOrAddController(handedness);
+                touchController.UpdateController(cameraRig, controller);
+            }
+            else
+            {
+                RemoveHandDevice(handedness);
+            }
+        }
+
+        private OculusQuestController GetOrAddController(Handedness handedness)
+        {
+            if (trackedControllers.ContainsKey(handedness))
+            {
+                return trackedControllers[handedness];
+            }
+
+            // Add new hand
+            var pointers = RequestPointers(SupportedControllerType.OculusTouch, handedness);
+            var inputSourceType = InputSourceType.Controller;
+
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+            var inputSource = inputSystem?.RequestNewGenericInputSource($"Oculus Quest {handedness} Controller", pointers, inputSourceType);
+
+            var controller = new OculusQuestController(TrackingState.Tracked, handedness, inputSource);
+            controller.SetupConfiguration(typeof(OculusQuestController));
+
+            for (int i = 0; i < controller.InputSource?.Pointers?.Length; i++)
+            {
+                controller.InputSource.Pointers[i].Controller = controller;
+            }
+
+            inputSystem?.RaiseSourceDetected(controller.InputSource, controller);
+
+            trackedControllers.Add(handedness, controller);
+
+            return controller;
+        }
+
+        private void RemoveControllerDevice(Handedness handedness)
+        {
+            if (trackedControllers.ContainsKey(handedness))
+            {
+                var hand = trackedControllers[handedness];
+                CoreServices.InputSystem?.RaiseSourceLost(hand.InputSource, hand);
+                trackedControllers.Remove(handedness);
+            }
+        }
+
+        private void RemoveAllControllerDevices()
+        {
+            foreach (var controller in trackedControllers.Values)
+            {
+                CoreServices.InputSystem?.RaiseSourceLost(controller.InputSource, controller);
+            }
+            trackedControllers.Clear();
+        }
+        #endregion
+
+        #region Hand Management
+        protected void UpdateHands()
+        {
+            UpdateHand(rightHand, rightSkeleton, Handedness.Right);
+            UpdateHand(leftHand, leftSkeleton, Handedness.Left);
+        }
+
+        protected void UpdateHand(OVRHand ovrHand, OVRSkeleton ovrSkeleton, Handedness handedness)
+        {
+            if (ovrHand.IsTracked)
+            {
+                var hand = GetOrAddHand(handedness);
+                hand.UpdateController(ovrHand, ovrSkeleton);
+            }
+            else
+            {
+                RemoveHandDevice(handedness);
+            }
+        }
+
+        private OculusQuestHand GetOrAddHand(Handedness handedness)
+        {
+            if (trackedHands.ContainsKey(handedness))
+            {
+                return trackedHands[handedness];
+            }
+
+            // Add new hand
+            var pointers = RequestPointers(SupportedControllerType.ArticulatedHand, handedness);
+            var inputSourceType = InputSourceType.Hand;
+
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+            var inputSource = inputSystem?.RequestNewGenericInputSource($"Oculus Quest {handedness} Hand", pointers, inputSourceType);
+
+            var controller = new OculusQuestHand(TrackingState.Tracked, handedness, inputSource);
+            controller.SetupConfiguration(typeof(OculusQuestHand));
+
+            for (int i = 0; i < controller.InputSource?.Pointers?.Length; i++)
+            {
+                controller.InputSource.Pointers[i].Controller = controller;
+            }
+
+            inputSystem?.RaiseSourceDetected(controller.InputSource, controller);
+
+            trackedHands.Add(handedness, controller);
+
+            return controller;
+        }
+
+        private void RemoveHandDevice(Handedness handedness)
+        {
+            if (trackedHands.ContainsKey(handedness))
+            {
+                var hand = trackedHands[handedness];
+                CoreServices.InputSystem?.RaiseSourceLost(hand.InputSource, hand);
+                trackedHands.Remove(handedness);
+            }
+        }
+
+        private void RemoveAllHandDevices()
+        {
+            foreach (var controller in trackedHands.Values)
+            {
+                CoreServices.InputSystem?.RaiseSourceLost(controller.InputSource, controller);
+            }
+            trackedHands.Clear();
+        }
+        #endregion
+    }
+}
